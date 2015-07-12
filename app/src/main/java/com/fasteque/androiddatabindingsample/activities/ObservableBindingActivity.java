7586ddc107c7e1a204b72f4cc7073911c42fd876
fasteque <daniele.altomare@gmail.com>
2015-07-12 16:31:57 +0200

    #1: stub views for Observable use case

---
+++ app/src/main/java/com/fasteque/androiddatabindingsample/activities/ObservableBindingActivity.java
@@ -0,0 +1,17 @@
+package com.fasteque.androiddatabindingsample.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fasteque.androiddatabindingsample.R;
+
+/**
+ * Created by daltomare on 12/07/15.
+ */
+public class ObservableBindingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observable_binding);
+    }
+}
