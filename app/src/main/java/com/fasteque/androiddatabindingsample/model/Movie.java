381663ac66696f3f8e13613033718ad7e2d23276
fasteque <daniele.altomare@gmail.com>
2015-07-14 21:45:03 +0200

    Implemented observable class

---
+++ app/src/main/java/com/fasteque/androiddatabindingsample/model/Movie.java
@@ -0,0 +1,46 @@
+package com.fasteque.androiddatabindingsample.model;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.fasteque.androiddatabindingsample.BR;
+
+/**
+ * Created by danielealtomare on 14/07/15.
+ * Project: AndroidDataBindingSample
+ */
+public class Movie extends BaseObservable {
+    private String title;
+    private String genre;
+    private int year;
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notifyPropertyChanged(BR.title);
+    }
+
+    @Bindable
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+        notifyPropertyChanged(BR.genre);
+    }
+
+    @Bindable
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+        notifyPropertyChanged(BR.year);
+    }
+}
