ee6de3ff4ecc577e910c210c2f3e4fa2698a2d1f
fasteque <daniele.altomare@gmail.com>
2015-07-15 23:56:50 +0200

    #1: observable data binding

--- app/src/main/java/com/fasteque/androiddatabindingsample/activities/ObservableBindingActivity.java
+++ app/src/main/java/com/fasteque/androiddatabindingsample/activities/ObservableBindingActivity.java
@@ -1,19 +1,35 @@
 package com.fasteque.androiddatabindingsample.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.fasteque.androiddatabindingsample.R;
+import com.fasteque.androiddatabindingsample.databinding.ObservableBinding;
+import com.fasteque.androiddatabindingsample.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by daltomare on 12/07/15.
  */
 public class ObservableBindingActivity extends BaseActivity {
+    private Button getMovieButton;
+    private Movie movie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_observable_binding);
+        ObservableBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_observable_binding);
+        movie = new Movie("Terminator Genisys", "Sci-Fi", 2015);
+        binding.setMovie(movie);
+
+        initToolbar();
+        initUserInterface();
     }
 
     @Override
@@ -36,5 +52,19 @@ public class ObservableBindingActivity extends BaseActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void initUserInterface() {
+        getMovieButton = (Button) findViewById(R.id.get_movie_button);
+        getMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dummyGetMovieLogic();
+            }
+        });
+    }
+
+    private void dummyGetMovieLogic() {
+        // TODO
     }
 }
