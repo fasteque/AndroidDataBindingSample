33971932f57817c25e9aef0d327146c0c606998f
fasteque <daniele.altomare@gmail.com>
2015-08-24 21:24:43 +0200

    #10: basic setup for Robolectric (still not working on API level 23)

---
+++ app/src/test/java/com/fasteque/androiddatabindingsample/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.fasteque.androiddatabindingsample;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MainActivityTest {
+    @Before
+    public void setUp() throws Exception {
+        // setup
+    }
+
+    @Test
+    public void testSomething() throws Exception {
+        // test
+    }
+}
