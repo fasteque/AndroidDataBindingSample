dded9eb0424c151b33d7db0840f9ec6e0140b373
fasteque <daniele.altomare@gmail.com>
2015-08-25 21:25:45 +0200

    #10: implemented base unit tests for all the activities

--- app/src/test/java/com/fasteque/androiddatabindingsample/RecyclerViewBindingActivityTest.java
+++ app/src/test/java/com/fasteque/androiddatabindingsample/RecyclerViewBindingActivityTest.java
@@ -1,6 +1,6 @@
 package com.fasteque.androiddatabindingsample;
 
-import com.fasteque.androiddatabindingsample.activities.MainActivity;
+import com.fasteque.androiddatabindingsample.activities.RecyclerViewBindingActivity;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,17 +13,19 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
-public class MainActivityTest {
+public class RecyclerViewBindingActivityTest {
 
-    private MainActivity activity;
+    private RecyclerViewBindingActivity activity;
 
     @Before
     public void setUp() throws Exception {
-        activity = Robolectric.setupActivity(MainActivity.class);
+        activity = Robolectric.setupActivity(RecyclerViewBindingActivity.class);
     }
 
     @Test
     public void titleIsCorrect() throws Exception {
-        assertTrue(activity.getTitle().toString().equals(activity.getString(R.string.app_name)));
+        System.out.println(activity.getString(R.string.observable_example));
+        assertTrue(activity.getSupportActionBar().getTitle().toString().equals(activity.getString(R.string
+                .recyclerview_example)));
     }
 }
