4db2d8744d39883e0e58832d63e5a3a819e55a56
fasteque <daniele.altomare@gmail.com>
2015-08-27 21:20:40 +0200

    #10: improved robolectric unit tests

--- app/src/test/java/com/fasteque/androiddatabindingsample/ObservableBindingActivityTest.java
+++ app/src/test/java/com/fasteque/androiddatabindingsample/ObservableBindingActivityTest.java
@@ -1,6 +1,8 @@
 package com.fasteque.androiddatabindingsample;
 
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.fasteque.androiddatabindingsample.activities.ObservableBindingActivity;
 
@@ -40,5 +42,17 @@ public class ObservableBindingActivityTest {
     public void layoutIsCorrect() throws Exception {
         final Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
         assertNotNull("toolbar is null", toolbar);
+
+        TextView text = (TextView) activity.findViewById(R.id.movieTitle);
+        assertNotNull("movieTitle is null", text);
+
+        text = (TextView) activity.findViewById(R.id.movieGenre);
+        assertNotNull("movieGenre is null", text);
+
+        text = (TextView) activity.findViewById(R.id.movieYear);
+        assertNotNull("movieYear is null", text);
+
+        final Button button = (Button) activity.findViewById(R.id.get_movie_button);
+        assertNotNull("get_movie_button is null", button);
     }
 }
