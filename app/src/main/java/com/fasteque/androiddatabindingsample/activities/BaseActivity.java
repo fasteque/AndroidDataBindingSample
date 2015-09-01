4bc9dce72cd0615c9fe51dd2b40f53cbdb689ebb
fasteque <daniele.altomare@gmail.com>
2015-09-01 22:20:16 +0200

    set proper access level

--- app/src/main/java/com/fasteque/androiddatabindingsample/activities/BaseActivity.java
+++ app/src/main/java/com/fasteque/androiddatabindingsample/activities/BaseActivity.java
@@ -9,7 +9,7 @@ import com.fasteque.androiddatabindingsample.R;
  * Created by daltomare on 13/07/15.
  */
 public abstract class BaseActivity extends AppCompatActivity {
-    protected Toolbar toolbar;
+    private Toolbar toolbar;
 
     protected void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
