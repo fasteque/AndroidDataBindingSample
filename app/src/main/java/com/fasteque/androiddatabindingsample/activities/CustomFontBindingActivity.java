bfb38e83bf65be41330f808ff2e65a91a8d4cde8
fasteque <daniele.altomare@gmail.com>
2015-09-05 14:10:55 +0200

    #11: custom font use case activity stub

---
+++ app/src/main/java/com/fasteque/androiddatabindingsample/activities/CustomFontBindingActivity.java
@@ -0,0 +1,50 @@
+package com.fasteque.androiddatabindingsample.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
+
+import com.fasteque.androiddatabindingsample.R;
+
+/**
+ * Created by daltomare on 12/07/15.
+ * Project: AndroidDataBindingSample
+ */
+public class CustomFontBindingActivity extends BaseActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_font);
+
+        initToolbar();
+    }
+
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(getString(R.string.custom_font_example));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
