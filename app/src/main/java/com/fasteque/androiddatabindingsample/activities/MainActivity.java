620503a293522d206b7b5f34123ec38ac1cc77b1
fasteque <daniele.altomare@gmail.com>
2015-09-28 21:17:17 +0200

    #13: set main activity menu

--- app/src/main/java/com/fasteque/androiddatabindingsample/activities/MainActivity.java
+++ app/src/main/java/com/fasteque/androiddatabindingsample/activities/MainActivity.java
@@ -1,11 +1,14 @@
 package com.fasteque.androiddatabindingsample.activities;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.fasteque.androiddatabindingsample.R;
@@ -44,7 +47,8 @@ public class MainActivity extends BaseActivity {
 
         NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
         view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
                 menuItem.setChecked(true);
                 drawerLayout.closeDrawers();
                 startBindingUseCase(menuItem);
@@ -55,11 +59,27 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
+        int id = item.getItemId();
+
+        if (id == R.id.action_attributions) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //noinspection unchecked
+                startActivity(new Intent(this, AttributionsActivity.class), ActivityOptions
+                        .makeSceneTransitionAnimation(this).toBundle());
+            } else {
+                startActivity(new Intent(this, AttributionsActivity.class));
+            }
+            return true;
+        } else if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
