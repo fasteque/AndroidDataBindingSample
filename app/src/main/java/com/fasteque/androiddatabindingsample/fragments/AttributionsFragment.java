378c84ebe7d73f8ed42a8b20fae9b3963e369f7a
fasteque <daniele.altomare@gmail.com>
2015-10-01 21:06:23 +0200

    #13: manually manage preference clicks

--- app/src/main/java/com/fasteque/androiddatabindingsample/fragments/AttributionsFragment.java
+++ app/src/main/java/com/fasteque/androiddatabindingsample/fragments/AttributionsFragment.java
@@ -1,9 +1,12 @@
 package com.fasteque.androiddatabindingsample.fragments;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import com.fasteque.androiddatabindingsample.R;
+import com.fasteque.androiddatabindingsample.activities.AttributionsActivity;
 
 /**
  * Created by danielealtomare on 28/09/15.
@@ -14,5 +17,15 @@ public class AttributionsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.attributions);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if(preference.getIntent() != null && preference.getIntent().getData() != null) {
+            ((AttributionsActivity) getActivity()).openCustomTab(preference.getIntent().getData());
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen,preference);
     }
 }
