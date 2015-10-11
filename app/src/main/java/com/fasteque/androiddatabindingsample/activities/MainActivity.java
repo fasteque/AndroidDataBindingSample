ff05289b4a08f049c25295757551f44f7652e28a
fasteque <daniele.altomare@gmail.com>
2015-10-11 21:29:17 +0200

    #15: set back the main application theme

--- app/src/main/java/com/fasteque/androiddatabindingsample/activities/MainActivity.java
+++ app/src/main/java/com/fasteque/androiddatabindingsample/activities/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         initToolbar();
